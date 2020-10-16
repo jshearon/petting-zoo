7fcbd10f9b052c487a3da681227ee611ae8d7a4f
Jonathan Shearon <jonathan.shearon@gmail.com>
2020-10-15 21:05:32 -0500

    created models

---
+++ animals.py
@@ -0,0 +1,38 @@
+from datetime import date
+
+from whale import Whale
+from shark import Shark
+from goldfish import Goldfish
+from eel import Eel
+from dolphin import Dolphin
+from donkey import Donkey
+from monkey import Monkey
+from llama import Llama
+from ostrich import Ostrich
+from tiger import Tiger
+from snake import Snake
+from lizard import Lizard
+from crocodile import Crocodile
+from turtle import Turtle
+from gecko import Gecko
+
+
+Bilbo = Whale("Bilbo", "Beluga Whale")
+Jaws = Shark("Jaws", "Great White Shark")
+Fabien = Goldfish("Fabien", "Goldfish")
+Saba = Eel("Saba", "Electric Eel")
+Alvin = Dolphin("Alvin", "Dolphin")
+
+Zeynep = Donkey("Zeynep", "Donkey")
+Zaine = Monkey("Zaine", "Capuchin Monkey")
+Anna = Ostrich("Anna", "Ostrich")
+Leonard = Llama("Leonard", "Llama")
+Bertie = Tiger("Bertie", "Bengal Tiger")
+
+Sherlock = Snake("Sherlock", "Python")
+Wilbur = Lizard("Wilbur", "Lizard")
+Madeline = Crocodile("Madeline", "Crocodile")
+Wilson = Turtle("Wilson", "Sea Turtle")
+Woody = Gecko ("Woody", "Gecko")
+
+print(Sherlock)
